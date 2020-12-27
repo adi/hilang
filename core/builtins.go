36940152bc20182534751aa5d9fafdbeb3e35724
Adrian Punga <adrian.punga@ejobsgroup.ro>
2020-12-28 00:28:15 +0200

    Add plus

--- core/builtins.go
+++ core/builtins.go
@@ -7,6 +7,46 @@ import (
 	"github.com/adi/hilang-routine/common"
 )
 
+// MathPlus ..
+var MathPlus = &common.Function{
+	Name: "+",
+	FixedArity: map[int]*common.Overload{
+		2: {
+			Params: []string{"x", "y"},
+			Code: func(env *common.Environment) (interface{}, error) {
+				if x, ok := env.Lookup("x"); ok {
+					if y, ok := env.Lookup("y"); ok {
+						switch x := x.(type) {
+						case float64:
+							switch y := y.(type) {
+							case float64:
+								return x + y, nil
+							case int64:
+								return x + float64(y), nil
+							default:
+								return nil, fmt.Errorf("+ takes 2 numeric parameters")
+							}
+						case int64:
+							switch y := y.(type) {
+							case float64:
+								return float64(x) + y, nil
+							case int64:
+								return x + y, nil
+							default:
+								return nil, fmt.Errorf("+ takes 2 numeric parameters")
+							}
+						default:
+							return nil, fmt.Errorf("+ takes 2 numeric parameters")
+						}
+					}
+					return nil, fmt.Errorf("internal error: parameter 'y' not found in environment")
+				}
+				return nil, fmt.Errorf("internal error: parameter 'x' not found in environment")
+			},
+		},
+	},
+}
+
 // Sqrt ..
 var Sqrt = &common.Function{
 	Name: "math#sqrt",
